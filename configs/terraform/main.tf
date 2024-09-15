8505d5ad8d09421111487fba51761b9745f2d602
SoheilStar <75124326+soheil-star01@users.noreply.github.com>
2024-09-15 15:25:38 +0300

    fix: add fixed wait time

--- configs/terraform/main.tf
+++ configs/terraform/main.tf
@@ -13,7 +13,3 @@ output "new_public_ip" {
   value = aws_instance.django_app.public_ip
   description = "The public IP address of the Django App instance"
 }
-output "instance_id" {
-  value = aws_instance.django_app.id
-  description = "The EC2 instance ID"
-}
