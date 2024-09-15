f18f47bfb49303f18851261b444083e786809eac
SoheilStar <75124326+soheil-star01@users.noreply.github.com>
2024-09-15 14:37:48 +0300

    fix: error with tf output

--- configs/terraform/main.tf
+++ configs/terraform/main.tf
@@ -1,16 +1,14 @@
 resource "aws_instance" "django_app" {
-  ami           = "ami-0e04bcbe83a83792e" # Use the correct AMI ID for your region
-  instance_type = "t2.micro"     # Specify the EC2 instance type here
+  ami           = "ami-0e04bcbe83a83792e"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "DjangoApp"
   }
 
-  # Ensure that the instance gets a public IP if it's in a VPC with an Internet Gateway
   associate_public_ip_address = true
 }
 
-# Output the public IP of the EC2 instance
 output "new_public_ip" {
   value = aws_instance.django_app.public_ip
   description = "The public IP address of the Django App instance"
