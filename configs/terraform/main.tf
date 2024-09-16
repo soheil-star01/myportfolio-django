4e0af42bbc587c487118ad4e282ad6017f7477f3
SoheilStar <75124326+soheil-star01@users.noreply.github.com>
2024-09-16 22:05:54 +0300

    fix: change ec2 os, modify docker installation

--- configs/terraform/main.tf
+++ configs/terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh8"
+  name        = "allow_ssh9"
   description = "Allow SSH inbound traffic"
 
   ingress {
@@ -27,7 +27,7 @@ resource "tls_private_key" "django_key_pair" {
 }
 
 resource "aws_key_pair" "django_key_pair" {
-  key_name   = "django-key8"
+  key_name   = "django-key9"
   public_key = tls_private_key.django_key_pair.public_key_openssh
 }
 
