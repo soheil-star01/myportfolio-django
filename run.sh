161f8bc0e3459e3668fe8d2e46ca10236fc1f919
SoheilStar <75124326+soheil-star01@users.noreply.github.com>
2024-09-25 22:29:32 +0300

    feature: adding https config back

--- run.sh
+++ run.sh
@@ -11,13 +11,8 @@ sleep 5
 
 gunicorn -c ./configs/gunicorn/gunicorn.conf.py
 
-cp /etc/nginx/conf.d/nginx.http.local.conf /etc/nginx/conf.d/default.conf
+certbot certonly --webroot --webroot-path=/var/www/certbot --email $LETSENCRYPT_EMAIL --agree-tos --no-eff-email -d samdolat.com -d www.samdolat.com
+
+cp /etc/nginx/conf.d/nginx.https.conf /etc/nginx/conf.d/default.conf
 
 nginx -s reload
-
-#
-#certbot certonly --webroot --webroot-path=/var/www/certbot --email $LETSENCRYPT_EMAIL --agree-tos --no-eff-email -d samdolat.com -d www.samdolat.com
-#
-#cp /etc/nginx/conf.d/nginx.https.conf /etc/nginx/conf.d/default.conf
-#
-#nginx -s reload
