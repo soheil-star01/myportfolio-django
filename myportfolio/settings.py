369c6326048e46b8dd043b7bc255aaf96aee9fd1
SoheilStar <75124326+soheil-star01@users.noreply.github.com>
2024-09-26 18:29:19 +0300

    feature: move media to s3 bucket

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -159,8 +159,9 @@ if os.environ.get('RUNNING_MODE') == 'Local':
 else:
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = 'django-portfolio-assets'
     AWS_S3_REGION_NAME = os.environ.get('AWS_REGION')
-    AWS_S3_CUSTOM_DOMAIN = 'django-portfolio-assets.s3.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     AWS_S3_URL_PROTOCOL = 'https'
     AWS_S3_USE_SSL = True
