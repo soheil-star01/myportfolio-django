8b119a298337225ff04452749a3a8ea8798f96dc
SoheilStar <75124326+soheil-star01@users.noreply.github.com>
2024-09-26 14:37:33 +0300

    fix: certbot directory

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,9 @@ RUN mkdir /django_app && cd /django_app
 WORKDIR /django_app
 
 COPY ./configs/nginx/nginx.http.conf /etc/nginx/nginx.conf
+COPY ./configs/nginx/nginx.https.conf /etc/nginx/nginx.https.conf
+COPY ./configs/nginx/certbot /var/www/certbot
+COPY ./configs/nginx/ssl /etc/letsencrypt
 
 COPY requirements.txt .
 
--- run.sh
+++ run.sh
@@ -11,12 +11,12 @@ sleep 5
 
 apt-get update && apt-get install -y certbot python3-certbot-nginx
 
-cp /etc/nginx/conf.d/nginx.http.conf /etc/nginx/conf.d/default.conf
+cp /etc/nginx/nginx.http.conf /etc/nginx/conf.d/default.conf
 nginx -s reload
 
 certbot certonly --webroot --webroot-path=/var/www/certbot --email $DJANGO_SUPERUSER_EMAIL --agree-tos --no-eff-email -d samdolat.com -d www.samdolat.com
 
-cp /etc/nginx/conf.d/nginx.https.conf /etc/nginx/conf.d/default.conf
+cp /etc/nginx/nginx.https.conf /etc/nginx/conf.d/default.conf
 
 nginx -s reload
 
