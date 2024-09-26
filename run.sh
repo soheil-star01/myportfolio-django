55c0aa96252a0f7d56f81813eb49ecaa746e4f21
SoheilStar <75124326+soheil-star01@users.noreply.github.com>
2024-09-26 15:33:57 +0300

    fix: issues with keeping cert

--- run.sh
+++ run.sh
@@ -11,16 +11,15 @@ sleep 5
 
 # Check if SSL certificates exist in S3
 if aws s3 ls s3://django-portfolio-assets/ssl-certs/fullchain.pem > /dev/null 2>&1; then
-  cp ./configs/nginx/nginx.http.conf /etc/nginx/conf.d/default.conf
-  nginx -s reload
-
   mkdir -p /etc/letsencrypt/live/samdolat.com
   echo "SSL certificates found on S3. Downloading..."
   aws s3 cp s3://django-portfolio-assets/ssl-certs/fullchain.pem /etc/letsencrypt/live/samdolat.com/fullchain.pem
   aws s3 cp s3://django-portfolio-assets/ssl-certs/privkey.pem /etc/letsencrypt/live/samdolat.com/privkey.pem
 else
+  cp ./configs/nginx/nginx.http.conf /etc/nginx/conf.d/default.conf
+  nginx -s reload
+
   echo "SSL certificates not found. Generating new ones with Certbot..."
-  apt-get update && apt-get install -y certbot python3-certbot-nginx
   certbot certonly --webroot --webroot-path=/var/www/certbot --email $DJANGO_SUPERUSER_EMAIL --agree-tos --no-eff-email -d samdolat.com -d www.samdolat.com
 
   # Upload new certificates to S3
