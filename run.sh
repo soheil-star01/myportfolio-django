d6e13fedd0995df929771c2e3e7ff62a469b7f44
SoheilStar <75124326+soheil-star01@users.noreply.github.com>
2024-09-26 14:21:03 +0300

    feat: test ssl certbot

--- run.sh
+++ run.sh
@@ -9,10 +9,15 @@ nginx -g 'daemon off;' &
 
 sleep 5
 
-gunicorn -c ./configs/gunicorn/gunicorn.conf.py
-
-#certbot certonly --webroot --webroot-path=/var/www/certbot --email $LETSENCRYPT_EMAIL --agree-tos --no-eff-email -d samdolat.com -d www.samdolat.com
+apt-get update && apt-get install -y certbot python3-certbot-nginx
 
 cp /etc/nginx/conf.d/nginx.http.conf /etc/nginx/conf.d/default.conf
+nginx -s reload
+
+certbot certonly --webroot --webroot-path=/var/www/certbot --email $DJANGO_SUPERUSER_EMAIL --agree-tos --no-eff-email -d samdolat.com -d www.samdolat.com
+
+cp /etc/nginx/conf.d/nginx.https.conf /etc/nginx/conf.d/default.conf
 
 nginx -s reload
+
+gunicorn -c ./configs/gunicorn/gunicorn.conf.py
