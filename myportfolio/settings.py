a3d4723775655b0b4b3f448bcb59e3a0159dd0a0
SoheilStar <75124326+soheil-star01@users.noreply.github.com>
2024-09-26 22:05:05 +0300

    fix: save on s3

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -163,12 +163,12 @@ else:
     AWS_S3_REGION_NAME = os.environ.get('AWS_REGION')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    AWS_S3_URL_PROTOCOL = 'https'
-    AWS_S3_USE_SSL = True
-    AWS_S3_VERIFY = True
+    AWS_DEFAULT_ACL = 'public-read'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
-    MEDIA_URL = f'{AWS_S3_URL_PROTOCOL}://{AWS_S3_CUSTOM_DOMAIN}/media/'
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    PUBLIC_MEDIA_LOCATION = 'media'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{PUBLIC_MEDIA_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'storage.PublicMediaStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
