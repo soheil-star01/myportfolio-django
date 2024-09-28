f8407c311aeb0dd6b6280fcb26c0d678c7f90373
SoheilStar <75124326+soheil-star01@users.noreply.github.com>
2024-09-28 16:15:23 +0300

    feature: cke5 upload storage

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
-# os.environ['RUNNING_MODE'] = 'Local'
+# os.environ['RUNNING_MODE'] = 'Local'  # TODO
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
