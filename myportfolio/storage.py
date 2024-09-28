cf7c0dfb8ae45ac636c1763ddc7b85e7b4e0931d
SoheilStar <75124326+soheil-star01@users.noreply.github.com>
2024-09-28 14:17:54 +0300

    feature: cke5 upload storage

--- myportfolio/storage.py
+++ myportfolio/storage.py
@@ -2,10 +2,11 @@ import os
 from urllib.parse import urljoin
 
 from django.conf import settings
-from django.core.files.storage import FileSystemStorage
+from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class CkeditorStorage(FileSystemStorage):
-
+class CkeditorS3BotoStorage(S3Boto3Storage):
+    default_acl = 'private'
+    file_overwrite = False
     location = os.path.join(settings.MEDIA_ROOT, "ckeditor")
     base_url = urljoin(settings.MEDIA_URL, "ckeditor/")
